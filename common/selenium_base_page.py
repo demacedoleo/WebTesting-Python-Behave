b40c338372a55f287c8bb2c34db45b188acaa4ad
Leonardo De Macedo <leo.unsta@gmail.com>
2015-10-11 10:31:23 -0300

    Create Archetype

--- common/selenium_base_page.py
+++ common/selenium_base_page.py
@@ -1,10 +1,15 @@
-__author__ = 'ldemacex'
+__author__ = 'Leonardo De Macedo'
 
 
-class BasePage(object):
+from selenium.webdriver import Firefox
+
+class BasePage(Firefox):
+
+    WEB_DRIVER_TIMEOUT = 15
 
     def __init__(self, web_driver):
         self.driver = web_driver.driver()
+        self.driver.implicitly_wait(self.WEB_DRIVER_TIMEOUT)
 
     def open_url(self, url):
         self.driver.get(url)
